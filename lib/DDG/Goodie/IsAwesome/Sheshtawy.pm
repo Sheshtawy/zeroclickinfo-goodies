8f4c9fb134c9031e785da132cdfad4df5f3beeb9
Sheshtawy <hishamelsheshtawy.fb@hotmail.com>
2015-03-13 22:34:26 +0000

    Created my first Goodie

--- lib/DDG/Goodie/IsAwesome/Sheshtawy.pm
+++ lib/DDG/Goodie/IsAwesome/Sheshtawy.pm
@@ -11,7 +11,7 @@ zci is_cached   => 1;
 # Metadata.  See https://duck.co/duckduckhack/metadata for help in filling out this section.
 name "IsAwesome Sheshtawy";
 description "My first Goodie, it lets the world know that GitHubUsername is awesome";
-primary_example_queries "duckduckhack GitHubUsername";
+primary_example_queries "duckduckhack Sheshtawy";
 category "special";
 topics "special_interest", "geek";
 attribution github => ["https://github.com/Sheshtawy", "Sheshtawy"],
